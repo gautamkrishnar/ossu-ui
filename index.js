551e7ed236939f0c71da9d8319a1f08152c89844
Joshua Humpherys <jbhum@outlook.com>
2015-10-12 15:09:02 -0400

    Add framework for landing page
    Modify .gitignore to include bundle.js and jsconfig.js. bundle.js is
    a compiled file, and jsconfig.js is a local config file.
    
    Modify package.json to add dependencies for babel, react, react-dom,
    and webpack.
    
    Add index.html and index.js as basic starting points for the landing
    page and the rest of the react app.
    
    Add webpack.config.js as configuration file for webpack. it targets
    index.js and compiles it and all it's dependencies using babel. The
    output is recorded in bundle.js. This file can be sent as a single self
    contained file to the client to render the app.

---
+++ index.js
@@ -0,0 +1,8 @@
+let React = require('react');
+let ReactDOM = require('react-dom');
+
+
+ReactDOM.render(
+	<h1>Hello, world!</h1>,
+	document.getElementById('example')
+);
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    entry: "./index.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.js?$/,  loader: 'babel?cacheDirectory' },
+            { test: /\.styl$/, loader: 'style-loader!css-loader!stylus-loader' },
+            { test: /\.css$/, loader: "style!css" }
+        ]
+    }
+};
