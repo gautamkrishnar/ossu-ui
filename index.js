2983501739c265d321a54183cd3c526b5efed9eb
Joshua Humpherys <jbhum@outlook.com>
2015-10-13 14:01:18 -0400

    Fix formatting issues per semistandard

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 let React = require('react');
 let ReactDOM = require('react-dom');
 
-
 ReactDOM.render(
-	<h1>Hello, world!</h1>,
+  <h1>Hello, world!</h1>,
 	document.getElementById('example')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    entry: "./index.js",
-    output: {
-        path: __dirname,
-        filename: "bundle.js"
-    },
-    module: {
-        loaders: [
-            { test: /\.js?$/,  loader: 'babel?cacheDirectory' },
-            { test: /\.styl$/, loader: 'style-loader!css-loader!stylus-loader' },
-            { test: /\.css$/, loader: "style!css" }
-        ]
-    }
+  entry: './index.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js?$/, loader: 'babel?cacheDirectory' },
+      { test: /\.styl$/, loader: 'style-loader!css-loader!stylus-loader' },
+      { test: /\.css$/, loader: 'style!css' }
+    ]
+  }
 };
