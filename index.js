de7dbf45667609c12b312e8a4c17a8066350eaf2
Brandon Stapleton <stapleton.brandon.r@gmail.com>
2015-10-14 17:33:06 -0400

    Sperate CSS from HTML; Import Font; Add animation
    Create a sperate file for css styles; add a prototype animation of OSSU
    logo using jquery and css animations. Import Questrial to Semantic Theme.

--- index.js
+++ index.js
@@ -69,7 +69,10 @@ class Masthead extends React.Component {
     return (
       <div className='mastHead ui inverted masthead center aligned segment'>
         <div className='ui text container'>
+          <h1 className='ui inverted header shadow'> OSSU </h1>
           <h1 className='ui inverted header'> OSSU </h1>
+        </div>
+        <div className='ui text container'>
           <h2> Learn Amazing things Here! </h2>
         </div>
       </div>
