2af9edd9c4f2a831843e8b460f6e0e4b5837a2e2
Joshua Humpherys <jbhum@outlook.com>
2015-10-19 00:06:00 -0400

    Restructure Files
    Create public/
    Create public/javascript/
    Move components/ to public/javascript/components
    Move semantic/ to public/semantic/
    Move favicons/ to public/favicons/
    Move all .js files except for index.js and bundle.js to
    public/javascript
    Modify import statements to correctly account for new file structure.

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import App from './components/App.js';
-import todoApp from './reducers';
+import App from './public/javascript/components/App.js';
+import todoApp from './public/javascript/reducers';
 
 let store = createStore(todoApp);
 
