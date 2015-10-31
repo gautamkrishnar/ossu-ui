8ee50b62a76ac2e085d7b11c6150b460b639093f
Brandon Stapleton <stapleton.brandon.r@gmail.com>
2015-10-30 21:39:17 -0400

    Needs to be updated to use props not local storage
    Code using local storage to check if user is logged in is commented out

--- public/javascript/components/NavLogo.js
+++ public/javascript/components/NavLogo.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 export class NavLogo extends React.Component {
+ 
+ /* Use props instead o local storage to retrieve
+ 
   isLoggedIn () {
     if (window.localStorage.getItem('jwt')) {
       let token = window.localStorage.getItem('jwt');
@@ -15,19 +18,31 @@ export class NavLogo extends React.Component {
     
     else {
       let navLeft = {
-      	name: 'Brandon ',
-      	picture: './public/favicons/mstile-70x70.png',
+      	name: ' ',
+      	picture: '/public/favicons/mstile-70x70.png',
       	href: '#',
       	id: 'logo-fade-in-out'
       }
       return navLeft;
     }
   }
+  
+  
+  render () {
+    return (
+      <a href={this.isLoggedIn().href}><div className='nav-text'>
+        <img id={this.isLoggedIn().id} className='nav-img' src={this.isLoggedIn().picture} /><p className='hide-on-mobile nav-text'> {this.isLoggedIn().name} </p>
+      </div></a>
+    );
+  }
+}
+
+*/
 
   render () {
     return (
-      <a href={this.isLoggedIn().href}><div className='nav-text' href='#'>
-        <img id={this.isLoggedIn().id} className='nav-img' src={this.isLoggedIn().picture} /><p className='hide-on-mobile nav-text'> {this.isLoggedIn().name} </p>
+      <a href='#'><div className='nav-text'>
+        <img id='logo-fad-in-out' className='nav-img' src='/public/favicons/mstile-70x70.png' /><p className='hide-on-mobile nav-text'>  </p>
       </div></a>
     );
   }
