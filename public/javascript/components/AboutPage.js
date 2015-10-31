effc647fb8162a736df06303c3cf3b9e1f7742a9
Joshua Humpherys <jbhum@outlook.com>
2015-10-31 15:28:19 -0400

    Update AboutPage.js
    AboutPage.js now better matches the new stylings.

--- public/javascript/components/AboutPage.js
+++ public/javascript/components/AboutPage.js
@@ -1,16 +1,22 @@
 import React, {Component} from 'react';
+import {VerticalSegment} from './VerticalSegment.js';
 
 export default class AboutPage extends Component {
   render () {
     return (
-      <div className='mastHead ui inverted masthead center aligned segment'>
-        <div className='ui text container'>
-          <h1 className='ui inverted header shadow' style={{color: 'White', marginTop: '2em'}}> This is an About page </h1>
+      <div>
+        <div className='mdl-layout__content masthead'>
+          <div className='page-header'>
+            <h1> OSS </h1>
+            <h2> Come learn about us </h2>
+          </div>
+          <div className='center-text-box constrain-text-box'>
+            <h3> An about page is a place that you can learn things about us! We appreciate the fact that you have visited our about page
+            and we really hope that in the process you have learned tons and tons about us! Never stop learning, That is my motto!</h3>
+          </div>
         </div>
-        <div className='ui text container'>
-          <h2> An about page is a place that you can learn things about us! We appreciate the fact that you have visited our about page
-          and we really hope that in the process you have learned tons and tons about us! Never stop learning, That is my motto!</h2>
-        </div>
+        <VerticalSegment />
+        <VerticalSegment />
       </div>
     );
   }
