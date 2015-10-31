fdca45e47691959437956d03f5c29622e95d9278
Brandon Stapleton <stapleton.brandon.r@gmail.com>
2015-10-30 21:31:50 -0400

    Update animations.js

--- public/javascript/animations.js
+++ public/javascript/animations.js
@@ -5,20 +5,19 @@ $(document).ready(function () {
 });
 
 function shadow() {
-	$('.masthead h1').addClass('text-shadow');
-	$('.masthead h2').addClass('text-shadow');
+  $('.masthead h1').addClass('text-shadow');
+  $('.masthead h2').addClass('text-shadow');
 }
 
 $(window).scroll(function() {     
-      var scroll = $(window).scrollTop();
-      var mastheadHeight = $(".masthead").height();
-      if (scroll > mastheadHeight - 56) {
-        $( ".mdl-layout__header--waterfall" ).addClass( "is-casting-shadow" );
-        $( "#logo-fade-in-out" ).addClass( "showing" );
-      }
-      else {
-        $( ".mdl-layout__header--waterfall" ).removeClass( "is-casting-shadow" );
-        $( "#logo-fade-in-out" ).removeClass( "showing" );
-       
-      }
+  var scroll = $(window).scrollTop();
+  var mastheadHeight = $(".masthead").height();
+  if (scroll > mastheadHeight - 56) {
+    $( ".mdl-layout__header--waterfall" ).addClass( "is-casting-shadow" );
+    $( "#logo-fade-in-out" ).addClass( "showing" );
+  }
+  else {
+    $( ".mdl-layout__header--waterfall" ).removeClass( "is-casting-shadow" );
+    $( "#logo-fade-in-out" ).removeClass( "showing" );
+  }
 });
