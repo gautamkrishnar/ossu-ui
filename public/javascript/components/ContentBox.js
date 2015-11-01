1f1f0329596499c5f0833abf184a6dd9a22bcb7c
Brandon Stapleton <stapleton.brandon.r@gmail.com>
2015-10-31 20:45:52 -0400

    Card height stays constant on window resize to break point. Looks better

--- public/javascript/components/ContentBox.js
+++ public/javascript/components/ContentBox.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Cell } from 'react-mdl/lib/Grid.js';
+import Card, { CardText, CardMenu, CardTitle, CardActions } from 'react-mdl/lib/Card/Card.js';
 
 export class ContentBox extends React.Component {
   
@@ -10,15 +11,16 @@ export class ContentBox extends React.Component {
   render () {
     return (
       <Cell col={4} tablet={6} phone={12} className='center-cell'>
-        <div className='mdl-card cell-width cast-shadow image-card'>
+        <Card className='cell-width cast-shadow image-card'>
           <div className='mdl-card__media'>
           	<img src={this.props.img} className='card-img' />
           </div>
-          <div className='mdl-card__title'>
+          <CardTitle>
             <h4 className='mdl-card__title-text'>{this.props.title}</h4>
-          </div>
-        <p> {this.props.blurb} </p> <a linkTo={this.props.link} > {this.props.linkText} </a>
-      </div>
+          </CardTitle>
+          <CardText  className='card-text'> {this.props.blurb} </CardText> 
+          <CardActions linkTo={this.props.link} > {this.props.linkText} </CardActions>
+      </Card>
     </Cell>
     );
   }
