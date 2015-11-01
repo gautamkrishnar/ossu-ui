189e6e033eb532d645c6b13284d0404570006ff9
Brandon Stapleton <stapleton.brandon.r@gmail.com>
2015-11-01 13:35:16 -0500

    Tabs convert to spaces, remove copy of main.css from public

--- public/javascript/components/ContentBox.js
+++ public/javascript/components/ContentBox.js
@@ -11,18 +11,18 @@ export class ContentBox extends React.Component {
   render () {
     return (
       <Cell col={4} tablet={7} phone={12} className='center-cell'>
-       <Cell col={12}>    
-        <Card className='cell-width cast-shadow image-card'>
-          <div className='mdl-card__media'>
-          	<img src={this.props.img} className='card-img' />
-          </div>
-          <CardTitle>
-            <h3>{this.props.title}</h3>
-          </CardTitle>
-          <CardText  className='card-text'> {this.props.blurb} </CardText> 
-          <CardActions linkTo={this.props.link} > {this.props.linkText} </CardActions>
-        </Card>
-       </Cell>
+        <Cell col={12}>    
+          <Card className='cell-width cast-shadow image-card'>
+            <div className='mdl-card__media'>
+              <img src={this.props.img} className='card-img' />
+            </div>
+            <CardTitle>
+              <h3>{this.props.title}</h3>
+            </CardTitle>
+            <CardText  className='card-text'> {this.props.blurb} </CardText> 
+            <CardActions linkTo={this.props.link} > {this.props.linkText} </CardActions>
+          </Card>
+        </Cell>
     </Cell>
     );
   }
