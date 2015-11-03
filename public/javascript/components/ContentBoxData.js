ea5a049da4dd1b9f1664c732e88afb0f4d1157ac
Joshua Humpherys <jbhum@outlook.com>
2015-11-03 15:48:08 -0500

    Clean up per Semistandard
    Mostly just formatting changes, but I got rid of eval(). I figured that
    there was another way to do what we were trying to do and we didn't need
    to call the compiler. Also Semistandard was whining about it.

--- public/javascript/components/ContentBoxData.js
+++ public/javascript/components/ContentBoxData.js
@@ -4,49 +4,69 @@ export class ContentBoxData extends React.Component {
 
   static getProps = function (name) {
     if (name !== undefined) {
-      let cardProps = 'this.' + name;
-        if (eval(cardProps) !== undefined ){
-          return eval(cardProps); 
-        }
-        else { 
-          return this.defaultProps;
-        }
-    }
-    else {
+      if (this.allProps[name] !== undefined) {
+        return this.allProps[name];
+      } else {
+        return this.defaultProps;
+      }
+    } else {
       return this.defaultProps;
     }
   }
 
+  static allProps = {
+    moocs: {
+      img: 'img/octocatvector.svg',
+      title: 'MOOCs',
+      blurb: 'Massively Open Online Courses.Free, open source classes from amazing universities.',
+      link: 'this should reveal a card with a more detailed description and a link to a larger decirption',
+      linkText: 'Click here to learn more'
+    },
+    github: {
+      img: 'img/octocatvector.svg',
+      title: 'Github',
+      blurb: "Github is 'Social Coding' Make your progress open source and share with the world!",
+      link: 'this should reveal a card with a more detailed description and a link to a larger decirption',
+      linkText: 'Click here to learn more'
+    },
+    projects: {
+      img: 'img/octocatvector.svg',
+      title: 'Projects',
+      blurb: "Learn by doing. Build a portifolio of projects showcasing what you've learned.",
+      link: 'this should reveal a card with a more detailed description and a link to a larger decirption',
+      linkText: 'Click here to learn more'
+    }
+  }
+
   static defaultProps = {
-        img:        'img/octocatvectr.svg',
-        title:      'Untitled',
-        blurb:      "This is an unused card that has no particular content. This will appear if no content is specified",
-        link:       "#",
-        linkText:   "Click for top of page"
+    img: 'img/octocatvectr.svg',
+    title: 'Untitled',
+    blurb: 'This is an unused card that has no particular content. This will appear if no content is specified',
+    link: '#',
+    linkText: 'Click for top of page'
   }
 
-  static moocs = {
-        img:        'img/octocatvector.svg',
-        title:      'MOOCs', 
-        blurb:      "Massively Open Online Courses.Free, open source classes from amazing universities.",
-        link:       "this should reveal a card with a more detailed description and a link to a larger decirption",
-        linkText:   "Click here to learn more"
-  }
+  // static moocs = {
+  //   img: 'img/octocatvector.svg',
+  //   title: 'MOOCs',
+  //   blurb: 'Massively Open Online Courses.Free, open source classes from amazing universities.',
+  //   link: 'this should reveal a card with a more detailed description and a link to a larger decirption',
+  //   linkText: 'Click here to learn more'
+  // }
 
-  static github = {
-        img:        'img/octocatvector.svg',
-        title:      'Github', 
-        blurb:      "Github is 'Social Coding' Make your progress open source and share with the world!",
-        link:       "this should reveal a card with a more detailed description and a link to a larger decirption",
-        linkText:   "Click here to learn more"
-  }
+  // static github = {
+  //   img: 'img/octocatvector.svg',
+  //   title: 'Github',
+  //   blurb: "Github is 'Social Coding' Make your progress open source and share with the world!",
+  //   link: 'this should reveal a card with a more detailed description and a link to a larger decirption',
+  //   linkText: 'Click here to learn more'
+  // }
 
-  static projects = {
-        img:        'img/octocatvector.svg',
-        title:      'Projects', 
-        blurb:      "Learn by doing. Build a portifolio of projects showcasing what you've learned.",
-        link:       "this should reveal a card with a more detailed description and a link to a larger decirption",
-        linkText:   "Click here to learn more"
-  }
-
+  // static projects = {
+  //   img: 'img/octocatvector.svg',
+  //   title: 'Projects',
+  //   blurb: "Learn by doing. Build a portifolio of projects showcasing what you've learned.",
+  //   link: 'this should reveal a card with a more detailed description and a link to a larger decirption',
+  //   linkText: 'Click here to learn more'
+  // }
 }
