279d94bd756b17ab26179e1dc926e3d17c5bfc74
Joshua Humpherys <jbhum@outlook.com>
2015-11-03 15:46:17 -0500

    Clean up according to SemiStandard
    all these files had some problems when it came to semistandard. A couple
    of them still had git merge trash still left in them and I removed that.

--- public/javascript/components/ContentBox.js
+++ public/javascript/components/ContentBox.js
@@ -1,29 +1,16 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Cell } from 'react-mdl/lib/Grid.js';
-import Card, { CardText, CardMenu, CardTitle, CardActions } from 'react-mdl/lib/Card/Card.js';
+import Card, { CardText, CardTitle, CardActions } from 'react-mdl/lib/Card/Card.js';
 
 export class ContentBox extends React.Component {
-  
   constructor (props) {
     super(props);
   }
 
   render () {
     return (
-<<<<<<< HEAD
-      <Cell col={4} tablet={12} phone={12}>
-      <div className='mdl-card cell-width cast-shadow image-card'>
-        <div className='mdl-card__media'>
-          <img src='img/octocatvector.svg' className='card-img' />
-        </div>
-        <div className='mdl-card__title'>
-        <h4 className='mdl-card__title-text'>Github</h4>
-        </div>
-        <p> Our curriculum is based around sharing github projects. Don't know what github is? <a href='https://help.github.com/articles/set-up-git/'> Click to learn more </a></p>
-      </div>
-=======
       <Cell col={4} tablet={7} phone={12} className='center-cell'>
-        <Cell col={12}>    
+        <Cell col={12}>
           <Card className='cell-width cast-shadow image-card'>
             <div className='mdl-card__media'>
               <img src={this.props.img} className='card-img' />
@@ -31,12 +18,19 @@ export class ContentBox extends React.Component {
             <CardTitle>
               <h3>{this.props.title}</h3>
             </CardTitle>
-            <CardText  className='card-text'> {this.props.blurb} </CardText> 
+            <CardText className='card-text'> {this.props.blurb} </CardText>
             <CardActions linkTo={this.props.link} > {this.props.linkText} </CardActions>
           </Card>
         </Cell>
->>>>>>> 189e6e033eb532d645c6b13284d0404570006ff9
     </Cell>
     );
   }
 }
+
+ContentBox.propTypes = {
+  img: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  blurb: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+  linkText: PropTypes.string.isRequired
+};
